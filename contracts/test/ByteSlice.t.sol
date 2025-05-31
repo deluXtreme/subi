d199befbf511933cb5f3cd05236203bf2d29fa18
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-05-31 11:58:47 +0200

    Cleanup event and test warnings (#13)
    Put subscription index at front of event.

--- contracts/test/ByteSlice.t.sol
+++ contracts/test/ByteSlice.t.sol
@@ -7,49 +7,49 @@ import {ByteSlice} from "src/libraries/ByteSlice.sol";
 contract ByteSliceTest is Test {
     using ByteSlice for bytes;
 
-    function testToIntSingleByte() public {
+    function testToIntSingleByte() public pure {
         bytes memory data = hex"01";
         uint256 result = data.toInt(0, 1);
         assertEq(result, 1);
     }
 
-    function testToIntTwoBytes() public {
+    function testToIntTwoBytes() public pure {
         bytes memory data = hex"0102";
         uint256 result = data.toInt(0, 2);
         assertEq(result, 0x0102);
     }
 
-    function testMaxU16() public {
+    function testMaxU16() public pure {
         bytes memory data = hex"ffffff";
         uint256 result = data.toInt(1, 3);
         assertEq(result, 0xffff);
     }
 
-    function testToIntThreeBytes() public {
+    function testToIntThreeBytes() public pure {
         bytes memory data = hex"010203";
         uint256 result = data.toInt(0, 3);
         assertEq(result, 0x010203);
     }
 
-    function testToIntMiddleSlice() public {
+    function testToIntMiddleSlice() public pure {
         bytes memory data = hex"0102030405";
         uint256 result = data.toInt(1, 4);
         assertEq(result, 0x020304);
     }
 
-    function testToIntMaxValue() public {
+    function testToIntMaxValue() public pure {
         bytes memory data = hex"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
         uint256 result = data.toInt(0, 32);
         assertEq(result, type(uint256).max);
     }
 
-    function testToIntZero() public {
+    function testToIntZero() public pure {
         bytes memory data = hex"000000";
         uint256 result = data.toInt(0, 3);
         assertEq(result, 0);
     }
 
-    function testToIntWithOffset() public {
+    function testToIntWithOffset() public pure {
         bytes memory data = hex"deadbeef";
         uint256 result = data.toInt(2, 4);
         assertEq(result, 0xbeef);
