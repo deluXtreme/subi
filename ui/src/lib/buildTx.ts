964c314f21d1cfec6d34cd94ba626895b8c1d086
0xpantera <0xpantera@proton.me>
2025-06-01 03:38:17 +0200

    add Wagmi sendCalls

--- ui/src/lib/buildTx.ts
+++ ui/src/lib/buildTx.ts
@@ -16,54 +16,101 @@ import {
   SUBSCRIPTION_MANAGER,
   SUBSCRIPTION_MASTER_COPY,
 } from "./constants";
+import { createLogger, logContractCall } from "./logger";
 
 const defaultSalt = BigInt(
   "110647465789069657756111682142268192901188952877020749627246931254533522453",
 );
+
+// Create logger for transaction building operations
+const logger = createLogger({ component: 'TransactionBuilder' });
 
 export async function prepareEnableModuleTransactions(
   safeAddress: Address,
   managerAddress: Address = SUBSCRIPTION_MANAGER,
   salt: bigint = defaultSalt,
 ): Promise<MetaTransactionData[]> {
+  logger.info('Starting module enablement transaction preparation', {
+    safeAddress,
+    managerAddress,
+    salt: salt.toString(),
+    hubAddress: HUB_ADDRESS,
+    moduleProxyFactory: MODULE_PROXY_FACTORY,
+    subscriptionMasterCopy: SUBSCRIPTION_MASTER_COPY
+  });
+
   const { tx: deployModuleTx, predictedAddress: moduleProxyAddress } =
     await buildModuleDeploymentTx(safeAddress, salt);
 
-  console.log(`Subscription Module address:`, moduleProxyAddress);
+  logger.info('Module deployment transaction prepared', {
+    safeAddress,
+    moduleProxyAddress,
+    deployTx: deployModuleTx
+  });
 
   // Prepare the meta-transaction data object
   const enableModuleTx = buildEnableModuleTx(safeAddress, moduleProxyAddress);
+  logger.debug('Enable module transaction built', { enableModuleTx });
+
   const registerModuleTx = buildRegisterManagerTx(
     moduleProxyAddress,
     managerAddress,
   );
+  logger.debug('Register module transaction built', { registerModuleTx });
+
   const moduleApprovalTx = buildModuleApprovalTx(
     HUB_ADDRESS,
     moduleProxyAddress,
   );
-  console.log("Deploy Tx", deployModuleTx);
-  console.log("Enable Tx", enableModuleTx);
-  console.log("Register Tx", registerModuleTx);
-  console.log("Approval Tx", moduleApprovalTx);
-  console.log("Predicted Address", moduleProxyAddress);
+  logger.debug('Module approval transaction built', { moduleApprovalTx });
 
-  return [deployModuleTx, enableModuleTx, registerModuleTx, moduleApprovalTx];
+  const transactions = [deployModuleTx, enableModuleTx, registerModuleTx, moduleApprovalTx];
+  
+  logger.info('All module enablement transactions prepared', {
+    safeAddress,
+    moduleProxyAddress,
+    transactionCount: transactions.length,
+    transactions: transactions.map((tx, index) => ({
+      index,
+      to: tx.to,
+      value: tx.value,
+      dataLength: tx.data.length
+    }))
+  });
+
+  return transactions;
 }
 
 export async function buildModuleDeploymentTx(
   safeAddress: Address,
   salt: bigint = defaultSalt,
 ): Promise<{ tx: MetaTransactionData; predictedAddress: Address }> {
+  logger.debug('Building module deployment transaction', {
+    safeAddress,
+    salt: salt.toString(),
+    masterCopy: SUBSCRIPTION_MASTER_COPY,
+    factory: MODULE_PROXY_FACTORY
+  });
+
   const initParams = encodeAbiParameters(
     parseAbiParameters("address x, address y, address z"),
     [safeAddress, safeAddress, safeAddress],
   );
+  logger.debug('Init parameters encoded', { initParams, safeAddress });
 
   const initData = encodeFunctionData({
     abi: parseAbi(["function setUp(bytes memory initParams)"]),
     functionName: "setUp",
     args: [initParams],
   });
+  logger.debug('Init data encoded', { initData });
+
+  logContractCall('ModuleProxyFactory', 'deployModule', {
+    masterCopy: SUBSCRIPTION_MASTER_COPY,
+    initializer: initData,
+    saltNonce: salt.toString()
+  });
+
   const deployData = encodeFunctionData({
     abi: parseAbi([
       "function deployModule(address masterCopy,bytes memory initializer, uint256 saltNonce) public returns (address proxy)",
@@ -71,26 +118,46 @@ export async function buildModuleDeploymentTx(
     functionName: "deployModule",
     args: [SUBSCRIPTION_MASTER_COPY, initData, salt],
   });
+
   const factoryAddress = MODULE_PROXY_FACTORY;
-  return {
+  const predictedAddress = await predictMinimalProxyAddress({
+    factory: MODULE_PROXY_FACTORY,
+    masterCopy: SUBSCRIPTION_MASTER_COPY,
+    initializer: initData, // as you've built above
+    saltNonce: salt,
+  });
+
+  const result = {
     tx: {
       to: factoryAddress.toString(),
       value: "0",
       data: deployData,
     },
-    predictedAddress: await predictMinimalProxyAddress({
-      factory: MODULE_PROXY_FACTORY,
-      masterCopy: SUBSCRIPTION_MASTER_COPY,
-      initializer: initData, // as you've built above
-      saltNonce: salt,
-    }),
+    predictedAddress,
   };
+
+  logger.info('Module deployment transaction built', {
+    safeAddress,
+    factoryAddress,
+    predictedAddress,
+    txData: {
+      to: result.tx.to,
+      value: result.tx.value,
+      dataLength: result.tx.data.length
+    }
+  });
+
+  return result;
 }
 
 function buildEnableModuleTx(
   safeAddress: Address,
   moduleAddress: Address,
 ): MetaTransactionData {
+  logger.debug('Building enable module transaction', { safeAddress, moduleAddress });
+
+  logContractCall('Safe', 'enableModule', { module: moduleAddress });
+
   // Build the call data for enabling the module
   const enableModuleData = encodeFunctionData({
     abi: parseAbi(["function enableModule(address module)"]),
@@ -98,39 +165,74 @@ function buildEnableModuleTx(
     args: [moduleAddress],
   });
 
-  // Prepare the meta-transaction data object
-  return {
+  const tx = {
     to: safeAddress,
     value: "0",
     data: enableModuleData,
   };
+
+  logger.debug('Enable module transaction built', {
+    safeAddress,
+    moduleAddress,
+    tx: {
+      to: tx.to,
+      value: tx.value,
+      dataLength: tx.data.length
+    }
+  });
+
+  // Prepare the meta-transaction data object
+  return tx;
 }
 
 function buildRegisterManagerTx(
   moduleAddress: Address,
   managerAddress: Address,
 ): MetaTransactionData {
+  logger.debug('Building register manager transaction', { moduleAddress, managerAddress });
+
+  logContractCall('SubscriptionManager', 'registerModule', { module: moduleAddress });
+
   // Build the call data for enabling the module
-  const enableModuleData = encodeFunctionData({
+  const registerModuleData = encodeFunctionData({
     abi: parseAbi(["function registerModule(address module)"]),
     functionName: "registerModule",
     args: [moduleAddress],
   });
 
-  // Prepare the meta-transaction data object
-  return {
+  const tx = {
     to: managerAddress,
     value: "0",
-    data: enableModuleData,
+    data: registerModuleData,
   };
+
+  logger.debug('Register manager transaction built', {
+    moduleAddress,
+    managerAddress,
+    tx: {
+      to: tx.to,
+      value: tx.value,
+      dataLength: tx.data.length
+    }
+  });
+
+  // Prepare the meta-transaction data object
+  return tx;
 }
 
 function buildModuleApprovalTx(
   hubAddress: Address,
   moduleProxyAddress: Address,
 ): MetaTransactionData {
+  logger.debug('Building module approval transaction', { hubAddress, moduleProxyAddress });
+
+  logContractCall('CirclesHub', 'setApprovalForAll', { 
+    operator: moduleProxyAddress, 
+    approved: true 
+  });
+
   // Build the call data for enabling the module
-  const enableModuleData = encodeFunctionData({
+  const approvalData = encodeFunctionData({
     abi: parseAbi([
       "function setApprovalForAll(address operator, bool approved)",
     ]),
@@ -138,12 +240,24 @@ function buildModuleApprovalTx(
     args: [moduleProxyAddress, true],
   });
 
-  // Prepare the meta-transaction data object
-  return {
+  const tx = {
     to: hubAddress,
     value: "0",
-    data: enableModuleData,
+    data: approvalData,
   };
+
+  logger.debug('Module approval transaction built', {
+    hubAddress,
+    moduleProxyAddress,
+    tx: {
+      to: tx.to,
+      value: tx.value,
+      dataLength: tx.data.length
+    }
+  });
+
+  // Prepare the meta-transaction data object
+  return tx;
 }
 
 function predictMinimalProxyAddress({
@@ -157,13 +271,27 @@ function predictMinimalProxyAddress({
   initializer: Hex;
   saltNonce: bigint | number;
 }): Address {
+  logger.debug('Predicting minimal proxy address', {
+    factory,
+    masterCopy,
+    initializerLength: initializer.length,
+    saltNonce: saltNonce.toString()
+  });
+
   // Salt: keccak256(abi.encodePacked(keccak256(initializer), saltNonce))
+  const initializerHash = keccak256(initializer);
   const salt = keccak256(
     encodePacked(
       ["bytes32", "uint256"],
-      [keccak256(initializer), BigInt(saltNonce)],
+      [initializerHash, BigInt(saltNonce)],
     ),
   );
+
+  logger.debug('Salt calculation', {
+    initializerHash,
+    saltNonce: saltNonce.toString(),
+    salt
+  });
 
   // Minimal proxy init code with masterCopy embedded
   const prefix = "0x602d8060093d393df3363d3d373d3d3d363d73";
@@ -176,9 +304,19 @@ function predictMinimalProxyAddress({
 
   const bytecodeHash = keccak256(initCode);
 
-  return getCreate2Address({
+  const predictedAddress = getCreate2Address({
     from: factory,
     salt,
     bytecodeHash,
   });
+
+  logger.info('Minimal proxy address predicted', {
+    factory,
+    masterCopy,
+    predictedAddress,
+    salt,
+    bytecodeHash
+  });
+
+  return predictedAddress;
 }
